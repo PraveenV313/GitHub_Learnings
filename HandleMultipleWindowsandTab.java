4fbfff5666a4c66eabc751e4e2cc0918bc59370a
PraveenV313 <86427100+PraveenV313@users.noreply.github.com>
2022-06-24 00:46:08 +0530

    Add files via upload
    To handle Multiple windows via Selenium code

---
+++ HandleMultipleWindowsandTab.java
@@ -0,0 +1,40 @@
+package LearnSelenium;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleMultipleWindowsandTab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\prave\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://leafground.com/pages/Window.html");
+		String oldwindow = driver.getWindowHandle();
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.navigate().to("http://leafground.com/pages/Button.html");
+		driver.switchTo().window(oldwindow);
+		
+		 String Currentwindow = driver.getWindowHandle(); 
+		 WebElement start = driver.findElement(By.xpath("//button[@id='home']")); 
+		 start.click();
+		 Set<String> newwindows = driver.getWindowHandles(); 
+		 for (String opennew : newwindows) 
+		 { 
+		 driver.switchTo().window(opennew); 
+		 } 
+		 WebElement newclick = driver.findElement(By.xpath("//*[@alt='Buttons']")); 
+		 newclick.click();
+		 //driver.quit(); 
+		 driver.switchTo().window(Currentwindow);
+		 
+		
+	}
+
+}
