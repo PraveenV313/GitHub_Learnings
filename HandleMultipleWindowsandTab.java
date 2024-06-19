d3f72b2ddd13a445d107a41c89052d939a741706
PraveenV313 <86427100+PraveenV313@users.noreply.github.com>
2024-06-19 21:34:31 +0530

    Update HandleMultipleWindowsandTab.java
    Jenkins webhook check

--- HandleMultipleWindowsandTab.java
+++ HandleMultipleWindowsandTab.java
@@ -32,7 +32,7 @@ public class HandleMultipleWindowsandTab {
 		 } 
 		 WebElement newclick = driver.findElement(By.xpath("//*[@alt='Buttons']")); 
 		 newclick.click();
-		 driver.quit(); 
+		 driver.close(); 
 		 driver.switchTo().window(Currentwindow);
 		 
 		
